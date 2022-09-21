254462e738d550447305fa857c209c6eaa014e2f
bgtti <bru.mgtti@gmail.com>
2022-09-21 11:24:02 +0200

    Updates

---
+++ precache-manifest.11e1109ed044811da674f12c034885d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee5aaa1617126b3c1b6",
+    "url": "/cv-creator/static/css/main.74475ecf.chunk.css"
+  },
+  {
+    "revision": "fee5aaa1617126b3c1b6",
+    "url": "/cv-creator/static/js/main.fee5aaa1.chunk.js"
+  },
+  {
+    "revision": "6a7bfa4df9242b40edff",
+    "url": "/cv-creator/static/js/1.6a7bfa4d.chunk.js"
+  },
+  {
+    "revision": "49be5e20e17b4aaa29ff",
+    "url": "/cv-creator/static/js/2.49be5e20.chunk.js"
+  },
+  {
+    "revision": "8283f5d237f36dc6af19",
+    "url": "/cv-creator/static/js/runtime~main.8283f5d2.js"
+  },
+  {
+    "revision": "b525259ab96e3b40af6feb4943ad4cb9",
+    "url": "/cv-creator/static/media/Image_CV.b525259a.jpg"
+  },
+  {
+    "revision": "cbd15d6f6130a37b3936ff0e6e4362fb",
+    "url": "/cv-creator/index.html"
+  }
+];
